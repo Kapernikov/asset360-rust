e1e4420e9f6711c0d9d92edd57e2cea486dcc0b2
Frank Dekervel <kervel@gmail.com>
2025-09-24 10:18:06 +0200

    Shrink Python wheel binaries

--- build_py3.13.sh
+++ build_py3.13.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cargo run --bin stub_gen
-docker run --rm -v $(pwd):/io ghcr.io/pyo3/maturin:latest   build --release --manylinux 2014 --interpreter /opt/python/cp313-cp313/bin/python
+docker run --rm -v $(pwd):/io ghcr.io/pyo3/maturin:latest \
+  build --release --strip --manylinux 2014 \
+  --interpreter /opt/python/cp313-cp313/bin/python
